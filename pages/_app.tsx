47d6166227936a34df5d519c6e003fa996f55f61
Mario Santos <mariodev7@gmail.com>
2022-01-25 19:06:49 -0300

    created app context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app'
+import AppProvider from '../contexts/App'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <AppProvider>
+            <Component {...pageProps} />
+        </AppProvider>
+    )
 }
 
 export default MyApp
