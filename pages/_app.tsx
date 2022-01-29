9f028ccb454fa8adc2c09bce6b7140e1b287d6df
Mario Santos <mariodev7@gmail.com>
2022-01-29 00:38:11 -0300

    setup firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app'
 import AppProvider from '../contexts/App'
+
+import '../services/firebase';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
