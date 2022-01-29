ccd1df33c85f3eed33d4cae90e6545c189a0f8ba
Mario Santos <mariodev7@gmail.com>
2022-01-29 20:12:03 -0300

    created page to edit/delete post

---
+++ pages/admin/post/edit/[id].tsx
@@ -0,0 +1,113 @@
+import { Fragment } from "react";
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
+import Head from 'next/head';
+
+import Header from "../../../../components/Navbar/Header";
+import Footer from "../../../../components/Footer";
+import Sidebar from "../../../../components/Navbar/Sidebar";
+import api, { createApiConnector } from "../../../../services/api";
+import PostForms from "../../../../components/Forms/Post";
+import { deleteImage, uploadImage } from "../../../../utils/firebase";
+import { useRouter } from "next/router";
+
+type PostsData = {
+    title: string;
+    projectURL: string;
+    repoURL: string;
+    isVisible: boolean;
+    technologies: string[];
+    description: string;
+}
+
+type PostFromDB = {
+    id: string;
+    title: string;
+    content?: string;
+    link?: string;
+    repository?: string;
+    published: boolean,
+    cover: string;
+    technologies: string[];
+}
+
+interface IEditPostProps {
+    post: PostFromDB;
+}
+
+export default function EditPost({ post }: IEditPostProps) {
+    const router = useRouter();
+
+    async function onUpdatePost(data: PostsData, file?: File) {
+        try {
+            let cover: string;
+
+            if (file) {
+                await deleteImage(post.cover);
+                cover = await uploadImage(file);
+            }
+
+            await api.put(`/api/post/${post.id}`, { ...data, cover });
+            alert('Post atualizado com sucesso.');
+        } catch (error: any) {
+            alert('Ocorreu um erro ao editar este post.')
+        }
+    }
+
+    async function onDeletePost(id: string) {
+        try {
+            await deleteImage(post.cover);
+            await api.delete(`/api/post/${id}`);
+
+            router.push('/admin');
+            alert('Post deletado com sucesso');
+        } catch (error: any) {
+            alert('Ocorreu um erro ao deletar este post.')
+        }
+    }
+
+    return (
+        <Fragment>
+            <Head>
+                <title>Editar Post | Dashboard Mário Portfolio</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Sidebar />
+            <div className="relative h-full transition-colors duration-500 bg-zinc-100 md:ml-64 dark:bg-slate-800">
+                <Header />
+                <div className="flex flex-col px-4 pt-4 mx-auto md:px-10">
+                    <div className="flex flex-wrap mt-4">
+                        <PostForms
+                            post={post}
+                            onUpdate={onUpdatePost}
+                            onDelete={onDeletePost}
+                        />
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </Fragment>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { ['@marioportfolio:token']: token } = parseCookies(ctx);
+    const { id } = ctx.query;
+
+    if (!token)
+        return {
+            redirect: {
+                destination: '/signin',
+                permanent: false
+            }
+        }
+
+    const api = createApiConnector(ctx);
+
+    const { data } = await api.get(`/api/post/${id}`);
+
+    return {
+        props: { post: data.post }
+    }
+}
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,4 +1,25 @@
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytes,
+    deleteObject
+} from "firebase/storage";
+
+function getPathStorageFromUrl(url: string) {
+    const baseUrlFirestore =
+        'https://firebasestorage.googleapis.com/v0/b/portfolio-c6350.appspot.com/o/';
+    if (!url.includes(baseUrlFirestore)) return false;
+
+    let imagePath = url.replace(baseUrlFirestore, '');
+
+    const indexOfEndPath = imagePath.indexOf('?');
+
+    imagePath = imagePath.substring(0, indexOfEndPath);
+    imagePath = imagePath.replace('%2F', '/');
+
+    return imagePath;
+}
 
 export function uploadImage(file: File) {
     return new Promise<string>(async (resolve, reject) => {
@@ -14,6 +35,26 @@ export function uploadImage(file: File) {
             const downloadURL = await getDownloadURL(snapshot.ref);
 
             resolve(downloadURL);
+        } catch (error: any) {
+            reject(error);
+        }
+    })
+}
+
+export function deleteImage(url: string) {
+    return new Promise<void>(async (resolve, reject) => {
+        try {
+            const pathOldImage = getPathStorageFromUrl(url);
+            const storage = getStorage();
+
+            if (pathOldImage) {
+                const storageRef = ref(storage, pathOldImage);
+
+                await deleteObject(storageRef);
+                resolve();
+            } else {
+                reject("Não foi encontrado imagem para deletar.");
+            }
         } catch (error: any) {
             reject(error);
         }
