24b746a33ae1bac20fa19f8bf96814ffbac3e7ae
Mario Santos <mariodev7@gmail.com>
2022-02-07 19:59:10 -0300

    added shimmer loading to pictures

--- components/Post/simple.tsx
+++ components/Post/simple.tsx
@@ -19,6 +19,25 @@ interface ISimplePost {
     onClick?: () => void;
 }
 
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str) =>
+    typeof window === 'undefined'
+        ? Buffer.from(str).toString('base64')
+        : window.btoa(str)
+
 export default function SimplePost({ post, onClick }: ISimplePost) {
     return (
         <div onClick={onClick} className="flex items-center justify-center w-full px-4 mt-4 md:w-1/2 lg:w-1/3">
@@ -30,8 +49,9 @@ export default function SimplePost({ post, onClick }: ISimplePost) {
                         alt={post.title}
                         objectFit='cover'
                         className="transition ease-out transform hover:scale-125 duration-2000"
-                        blurDataURL="data:..."
+                        blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
                         placeholder="blur"
+
                     />
                     <h1 className='absolute text-lg text-white bottom-1 left-1'>{post.title}</h1>
                 </div>
