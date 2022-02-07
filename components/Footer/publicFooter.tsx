13104029ba68fed6e5fa01356c1b5b5413c69720
Mario Santos <mariodev7@gmail.com>
2022-02-07 17:46:44 -0300

    style: fixed align margins

--- components/Footer/publicFooter.tsx
+++ components/Footer/publicFooter.tsx
@@ -2,20 +2,22 @@ import Image from "next/image";
 
 export default function PublicFooter() {
     return (
-        <footer className='flex flex-col items-center py-4 md:justify-around md:flex-row'>
-            <p>
-                © Mário Santos. All Rights Reserved.
-            </p>
-            <a
-                href="https://vercel.com"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{' '}
-                <span>
-                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </span>
-            </a>
+        <footer className='flex justify-center py-4'>
+            <div className="flex flex-col items-center w-full max-w-5xl md:justify-between md:flex-row">
+                <p>
+                    © Mário Santos. All Rights Reserved.
+                </p>
+                <a
+                    href="https://vercel.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <span>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    </span>
+                </a>
+            </div>
         </footer>
     )
 }
