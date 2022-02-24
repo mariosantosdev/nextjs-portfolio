059be9a1109c9f16a041ac761d663777f8426bb5
Mario Santos <mariodev7@gmail.com>
2022-02-24 18:52:38 -0300

    style: refactored styles to responsive usage

--- components/Navbar/PageLink.tsx
+++ components/Navbar/PageLink.tsx
@@ -2,25 +2,27 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export interface IPropsPageLink {
-    title: string;
-    path: string;
+  title: string;
+  path: string;
 }
 
 export default function PageLink({ title, path }: IPropsPageLink) {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <li
-            className={`
-                flex justify-center w-full text-xl md:text-md transition-all duration-300 hover:text-xl
-                ${(router.pathname === path)
-                    ? ' dark:text-indigo-300 text-indigo-500'
-                    : ' dark:text-white text-gray-600'}
+  return (
+    <li
+      className={`
+                flex justify-center md:w-1/4 md:text-md w-full text-xl transition-all duration-300 hover:text-xl
+                ${
+                  router.asPath === path || router.asPath === `/${path}`
+                    ? " text-indigo-500"
+                    : " text-white"
+                }
                 `}
-        >
-            <Link href={path}>
-                <a>{title}</a>
-            </Link>
-        </li>
-    )
+    >
+      <Link href={path}>
+        <a>{title}</a>
+      </Link>
+    </li>
+  );
 }
