917089d6c90dd123d41dbc940f6192e3d459f07d
Mario Santos <mariodev7@gmail.com>
2022-02-24 21:56:24 -0300

    feat: exporting type Post

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,3 +1,16 @@
-import SimplePost from "./simple";
+import SimplePost from './simple';
+
+export type Post = {
+  id: string;
+  title: string;
+  content?: string;
+  link?: string;
+  repository?: string;
+  published: boolean;
+  cover: string;
+  technologies: string[];
+  createdAt: string;
+  updatedAt: string;
+};
 
 export default SimplePost;
