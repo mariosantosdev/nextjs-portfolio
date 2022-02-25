a5f8150d6d0725cf573d9893c6dbb1cbf77acd21
Mario Santos <mariodev7@gmail.com>
2022-02-24 23:32:22 -0300

    fix(get post): fixed tratate query params to limit

--- pages/api/post.tsx
+++ pages/api/post.tsx
@@ -64,9 +64,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         switch (req.method) {
             case 'GET':
                 const { limit } = req.query;
-                const limitToFetch = Number(
-                typeof limit === 'string' ? limit : limit[0]
-                );
+                const limitToFetch =
+                        limit && Number(typeof limit === 'string' ? limit : limit[0])
                 const posts = await getPosts(limitToFetch);
                 res.status(200).json({ posts });
                 break;
