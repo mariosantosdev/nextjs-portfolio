d60147cdcfb7efe47fc05eb64e62eccb1cdf6ec8
Mario Santos <mariodev7@gmail.com>
2022-02-24 21:47:00 -0300

    style(footer): added bg-white

--- components/Footer/publicFooter.tsx
+++ components/Footer/publicFooter.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function PublicFooter() {
   return (
-    <footer className="flex justify-center py-4">
+    <footer className="flex justify-center w-full py-4 bg-white">
       <div className="flex flex-col items-center w-full max-w-5xl md:px-4 md:justify-between md:flex-row">
         <p>© Mário Santos. All Rights Reserved.</p>
         <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
