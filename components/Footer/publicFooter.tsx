ba4163cda76e0b0631d29882fe782cfdc68dbea9
Mario Santos <mariodev7@gmail.com>
2022-02-24 21:29:20 -0300

    style: added padding y

--- components/Footer/publicFooter.tsx
+++ components/Footer/publicFooter.tsx
@@ -1,23 +1,17 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function PublicFooter() {
-    return (
-        <footer className='flex justify-center py-4'>
-            <div className="flex flex-col items-center w-full max-w-5xl md:justify-between md:flex-row">
-                <p>
-                    © Mário Santos. All Rights Reserved.
-                </p>
-                <a
-                    href="https://vercel.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <span>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
-                </a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="flex justify-center py-4">
+      <div className="flex flex-col items-center w-full max-w-5xl md:px-4 md:justify-between md:flex-row">
+        <p>© Mário Santos. All Rights Reserved.</p>
+        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
+          Powered by{' '}
+          <span>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </div>
+    </footer>
+  );
 }
