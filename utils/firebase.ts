cc6ed7e940d57167cd92f2e9eb14edf5de14ab7b
Mário Santos <mariodev7@gmail.com>
2022-05-04 18:53:22 -0300

    refactor(firebase): using app to uplaod image

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,62 +1,69 @@
 import {
-    getDownloadURL,
-    getStorage,
-    ref,
-    uploadBytes,
-    deleteObject
-} from "firebase/storage";
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytes,
+  deleteObject,
+  UploadMetadata,
+} from 'firebase/storage';
+import { initFirebase } from '../services/firebase';
 
 function getPathStorageFromUrl(url: string) {
-    const baseUrlFirestore =
-        'https://firebasestorage.googleapis.com/v0/b/portfolio-c6350.appspot.com/o/';
-    if (!url.includes(baseUrlFirestore)) return false;
+  const baseUrlFirestore =
+    'https://firebasestorage.googleapis.com/v0/b/portfolio-c6350.appspot.com/o/';
+  if (!url.includes(baseUrlFirestore)) return false;
 
-    let imagePath = url.replace(baseUrlFirestore, '');
+  let imagePath = url.replace(baseUrlFirestore, '');
 
-    const indexOfEndPath = imagePath.indexOf('?');
+  const indexOfEndPath = imagePath.indexOf('?');
 
-    imagePath = imagePath.substring(0, indexOfEndPath);
-    imagePath = imagePath.replace('%2F', '/');
+  imagePath = imagePath.substring(0, indexOfEndPath);
+  imagePath = imagePath.replace('%2F', '/');
 
-    return imagePath;
+  return imagePath;
 }
 
 export function uploadImage(file: File) {
-    return new Promise<string>(async (resolve, reject) => {
-        try {
-            const storage = getStorage();
+  const app = initFirebase();
 
-            // create a storage ref
-            const storageRef = ref(storage, 'covers/' + file.name);
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      const storage = getStorage(app);
 
-            // upload file
-            const snapshot = await uploadBytes(storageRef, file);
+      // create a storage ref
+      const storageRef = ref(storage, 'covers/' + file.name);
 
-            const downloadURL = await getDownloadURL(snapshot.ref);
+      // upload file
+      const metadata: UploadMetadata = { contentType: file.type };
+      const snapshot = await uploadBytes(storageRef, file, metadata);
 
-            resolve(downloadURL);
-        } catch (error: any) {
-            reject(error);
-        }
-    })
+      const downloadURL = await getDownloadURL(snapshot.ref);
+
+      resolve(downloadURL);
+    } catch (error: any) {
+      reject(error);
+    }
+  });
 }
 
 export function deleteImage(url: string) {
-    return new Promise<void>(async (resolve, reject) => {
-        try {
-            const pathOldImage = getPathStorageFromUrl(url);
-            const storage = getStorage();
+  const app = initFirebase();
 
-            if (pathOldImage) {
-                const storageRef = ref(storage, pathOldImage);
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      const pathOldImage = getPathStorageFromUrl(url);
+      const storage = getStorage(app);
 
-                await deleteObject(storageRef);
-                resolve();
-            } else {
-                reject("Não foi encontrado imagem para deletar.");
-            }
-        } catch (error: any) {
-            reject(error);
-        }
-    })
+      if (pathOldImage) {
+        const storageRef = ref(storage, pathOldImage);
+
+        await deleteObject(storageRef);
+        resolve();
+      } else {
+        reject('Não foi encontrado imagem para deletar.');
+      }
+    } catch (error: any) {
+      reject(error);
+    }
+  });
 }
