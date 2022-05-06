a11dc7465566731bdc6d185e11102497ed1d11a6
Mário Santos <mariodev7@gmail.com>
2022-05-06 10:44:39 -0300

    style(404): responsive screen

--- pages/404.tsx
+++ pages/404.tsx
@@ -13,13 +13,18 @@ export default function Home() {
         path={router.asPath}
       />
 
-      <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-r from-indigo-600 to-blue-400">
-        <div className="px-40 py-20 bg-white rounded-md shadow-xl">
+      <div className="flex items-center justify-center w-screen h-screen px-2 bg-gradient-to-r from-indigo-600 to-indigo-400">
+        <div className="px-4 py-10 bg-white rounded-md shadow-xl md:px-40 md:py-20">
           <div className="flex flex-col items-center">
-            <h1 className="font-bold text-indigo-600 text-9xl">404</h1>
+            <h1 className="font-bold text-indigo-600 text-8xl md:text-9xl">
+              404
+            </h1>
 
             <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-              <span className="text-red-500">Oops!</span> Página não encontrada
+              <p className="text-lg">
+                <span className="text-red-500">Oops!</span> Página não
+                encontrada
+              </p>
             </h6>
 
             <p className="mb-8 text-center text-gray-500 md:text-lg">
